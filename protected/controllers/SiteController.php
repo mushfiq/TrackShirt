35469b8fbe2bd1f318bb8eddce77360d0155ff17
Mohammed Mushfiq <mushfiq@newscred.com>
2011-12-30 16:57:57 +0600

    cleaned the project

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -19,31 +19,6 @@ class SiteController extends Controller {
             ),
         );
     }
-//
-//    public function actionIndex() {
-//        $model = new HideForm;
-//        if (isset($_POST['HideForm'])) {
-//            $hide = new Hide;
-//            $model->attributes = $_POST['HideForm'];
-//            if ($model->validate()) {
-//                try {
-//                    $hide->url = $_POST['HideForm']['url'];
-//                    $hide->create_time = time();
-//                    $hide->save();
-//                    $read = Hide::model()->find('create_time=:create_time', array(':create_time' => $hide->create_time));
-//                    $test["url"] = $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'].'site/redirect/?id=';
-//                    $test["id"] = $read->id;
-//                    $this->render('success', $test);
-//                } catch (Exception $e) {
-//                    echo 'Caught exception: ', $e->getMessage(), "\n";
-//                }
-//            }
-//        }
-//        else{
-//            $this->render('hide', array('model' => $model));
-//        }
-//        
-//    }
 
     /**
      * This is the action to handle external exceptions.
@@ -67,100 +42,55 @@ class SiteController extends Controller {
         }
         $this->render('redirect', $test);
     }
-    
-    
-    
-    public function actionIndex(){
+
+    public function actionIndex() {
         $model = new TrackSearchForm;
-        if(isset($_POST['TrackSearchForm'])){
+        if (isset($_POST['TrackSearchForm'])) {
             print "here";
             die;
         }
-        $this->render('details',array('model' => $model));
-        
-        
+        $this->render('details', array('model' => $model));
     }
-//    
-//    public function actionDetails(){
-//        
-//       $this->render('index');
-//    }
-    public function actionEntryshirt(){
-        $model = new TrackEntryForm;
-        
-        
-         if (isset($_POST['TrackEntryForm'])) {
-             echo "go go!";
-            $tracjentry = new TrackEntry  ;
-            $model->attributes = $_POST['TrackEntryForm'];
-            $this->render('success', $test);
-            var_dump($model->attributes);
-            
-//            if ($model->validate()) {
-//                try {
-//                    $hide->url = $_POST['HideForm']['url'];
-//                    $hide->create_time = time();
-//                    $hide->save();
-//                    $read = Hide::model()->find('create_time=:create_time', array(':create_time' => $hide->create_time));
-//                    $test["url"] = $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'].'site/redirect/?id=';
-//                    $test["id"] = $read->id;
-//                    $this->render('success', $test);
-//                } catch (Exception $e) {
-//                    echo 'Caught exception: ', $e->getMessage(), "\n";
-//                }
-//            }
-        }
-        
-        
-        
-        
-        
-        
-        
-        
-        $this->render('entry-shirt',array('model' => $model));
+
+    public function actionDetails() {
+
+        $this->render('details-product');
     }
-    
-     public function actionDetails(){
-         
-         $this->render('details-product');
-        
-    }
-    
-    public function actionTest(){
+
+    public function actionEntry() {
         $model = new TrackForm;
-        
-          $this->render('test', array('model' => $model));
-          if(isset($_POST['TrackForm'])){
-              $entry = new Track;
-           print_r($_POST);
+
+
+        if (isset($_POST['TrackForm'])) {
+            $entry = new Track;
+            print_r($_POST);
 //           die;
-              
 //                          if ($model->validate()) {
-                try {
-                    $entry->lot_number = $_POST['TrackForm']['lot_number'];
-                    $entry->print_date = $_POST['TrackForm']['print_date'];
-                    
-                    $entry->print_location = $_POST['TrackForm']['print_location'];
-                    $entry->contact_bussiness = $_POST['TrackForm']['contact_bussiness'];
-                    
-                    $entry->contact_city = $_POST['TrackForm']['contact_city'];
-                    $entry->contact_state = $_POST['TrackForm']['contact_state'];
-                    $entry->contact_zip_code = $_POST['TrackForm']['contact_zip_code'];
-                    $entry->contact_phone = $_POST['TrackForm']['contact_phone'];
+            try {
+                $entry->lot_number = $_POST['TrackForm']['lot_number'];
+                $entry->print_date = $_POST['TrackForm']['print_date'];
+
+                $entry->print_location = $_POST['TrackForm']['print_location'];
+                $entry->contact_bussiness = $_POST['TrackForm']['contact_bussiness'];
+
+                $entry->contact_city = $_POST['TrackForm']['contact_city'];
+                $entry->contact_state = $_POST['TrackForm']['contact_state'];
+                $entry->contact_zip_code = $_POST['TrackForm']['contact_zip_code'];
+                $entry->contact_phone = $_POST['TrackForm']['contact_phone'];
 //                    $entry->contact_city = $_POST['TrackForm']['contact_city'];
 //                    $entry->contact_city = $_POST['TrackForm']['contact_city'];
 //                    $entry->contact_city = $_POST['TrackForm']['contact_city'];
 //                    $entry->contact_city = $_POST['TrackForm']['contact_city'];
-                    $entry->save();
-                    print "sucess!";
-                    $this->render('success', array('model' => $model));
-                } catch (Exception $e) {
-                    echo 'Caught exception: ', $e->getMessage(), "\n";
-                }
-//                          }
+                $entry->save();
+                print "sucess!";
+                $this->render('success', array('model' => $model));
+            } catch (Exception $e) {
+                echo 'Caught exception: ', $e->getMessage(), "\n";
             }
-          
+//                          }
+        } else {
+            $this->render('entry', array('model' => $model));
+        }
     }
 
 }
